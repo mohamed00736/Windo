de13918ee5752d3c4b24d5b3346a3f6aaf2712f4
HAKIM HACINE <mohamed.hacine00@gmail.com>
2025-09-17 18:43:53 +0100

    Initial Commit

---
+++ Windo/WindoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WindoApp.swift
+//  Windo
+//
+//  Created by hacinemed on 17/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct WindoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
